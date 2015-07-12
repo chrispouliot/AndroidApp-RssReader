20d073516f04a9088182c3ffee14630169d88b2e
Christophe Pouliot <cristophepoug@gmail.com>
2015-07-12 15:13:19 -0700

    sql part 2

--- app/src/main/java/com/android/acios/blocly/api/DataSource.java
+++ app/src/main/java/com/android/acios/blocly/api/DataSource.java
@@ -1,7 +1,14 @@
 package com.android.acios.blocly.api;
 
+import android.database.sqlite.SQLiteDatabase;
+
+import com.android.acios.blocly.BloclyApplication;
+import com.android.acios.blocly.BuildConfig;
 import com.android.acios.blocly.api.model.RssFeed;
 import com.android.acios.blocly.api.model.RssItem;
+import com.android.acios.blocly.api.model.database.DatabaseOpenHelper;
+import com.android.acios.blocly.api.model.database.table.RssFeedTable;
+import com.android.acios.blocly.api.model.database.table.RssItemTable;
 import com.android.acios.blocly.api.network.GetFeedsNetworkRequest;
 
 import java.util.ArrayList;
@@ -9,17 +16,28 @@ import java.util.List;
 
 public class DataSource {
 
+    private DatabaseOpenHelper databaseOpenHelper;
+    private RssFeedTable rssFeedTable;
+    private RssItemTable rssItemTable;
     private List<RssFeed> feeds;
     private List<RssItem> items;
 
     public DataSource() {
+
+        rssFeedTable = new RssFeedTable();
+        rssItemTable = new RssItemTable();
+        databaseOpenHelper = new DatabaseOpenHelper(BloclyApplication.getSharedInstance(), rssFeedTable, rssItemTable);
+
         feeds = new ArrayList<>();
         items = new ArrayList<>();
-        // I assume I do List<FeedResponse> = new GetFeedsNetworkRequest(...)
-        // then just set feeds / items to the private ones
+
         new Thread(new Runnable() {
             @Override
             public void run() {
+                if (BuildConfig.DEBUG && false) {
+                    BloclyApplication.getSharedInstance().deleteDatabase("blocly_db");
+                }
+                SQLiteDatabase writableDatabase = databaseOpenHelper.getWritableDatabase();
                 new GetFeedsNetworkRequest("http://feeds.feedburner.com/androidcentral?format=xml").performRequest();
             }
         }).start();
