a0307c493560d73c5f441f4fc941d8f6a7d2c345
Christophe Pouliot <cristophepoug@gmail.com>
2015-07-12 13:52:15 -0700

    GET MORE RSS FEEDS

--- app/src/main/java/com/android/acios/blocly/api/DataSource.java
+++ app/src/main/java/com/android/acios/blocly/api/DataSource.java
@@ -1,7 +1,5 @@
 package com.android.acios.blocly.api;
 
-import com.android.acios.blocly.BloclyApplication;
-import com.android.acios.blocly.R;
 import com.android.acios.blocly.api.model.RssFeed;
 import com.android.acios.blocly.api.model.RssItem;
 import com.android.acios.blocly.api.network.GetFeedsNetworkRequest;
@@ -15,10 +13,10 @@ public class DataSource {
     private List<RssItem> items;
 
     public DataSource() {
-        feeds = new ArrayList<RssFeed>();
-        items = new ArrayList<RssItem>();
-        createFakeData();
-
+        feeds = new ArrayList<>();
+        items = new ArrayList<>();
+        // I assume I do List<FeedResponse> = new GetFeedsNetworkRequest(...)
+        // then just set feeds / items to the private ones
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -35,17 +33,5 @@ public class DataSource {
         return items;
     }
 
-    void createFakeData() {
-        feeds.add(new RssFeed("My Favorite Feed",
-                "This feed is just incredible, I can't even begin to tell you…",
-                "http://favoritefeed.net", "http://feeds.feedburner.com/favorite_feed?format=xml"));
-        for (int i = 0; i < 10; i++) {
-            items.add(new RssItem(String.valueOf(i),
-                    BloclyApplication.getSharedInstance().getString(R.string.placeholder_headline),
-                    BloclyApplication.getSharedInstance().getString(R.string.placeholder_content),
-                    "http://favoritefeed.net?story_id=an-incredible-news-story",
-                    "http://rs1img.memecdn.com/silly-dog_o_511213.jpg",
-                    0, System.currentTimeMillis(), false, false, false));
-        }
-    }
+
 }
